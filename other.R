35f4b9f8b78bf82c4bc4d7e9a28e93fa5dabbaa8
Jason Fisher <jfisher@usgs.gov>
2020-05-09 22:37:14 -0700

    add partial match

--- other.R
+++ other.R
@@ -1,8 +1,9 @@
-
 .help <- function(package) {
   if (!tryCatch(is.character(package) && length(package) == 1L, error=function(e) FALSE)) {
     package <- deparse1(substitute(package))
   }
+  choices <- as.character(utils::installed.packages()[, "Package"])
+  package <- match.arg(package, choices)
   suppressPackageStartupMessages(library((package), character.only=TRUE, quietly=TRUE))
   suppressMessages(utils::help(package=(package), help_type="html"))
 }
