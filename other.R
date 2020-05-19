8628b72a8e4c81466f14b85cdfb3a6a6fc3be3fd
Jason Fisher <jfisher@usgs.gov>
2020-05-19 11:47:56 -0700

    change name from phelp to ph

--- other.R
+++ other.R
@@ -1,9 +1,9 @@
-phelp <- function(package) {
+ph <- function(package) {
   if (!tryCatch(is.character(package) && length(package) == 1L, error=function(e) FALSE)) {
     package <- deparse1(substitute(package))
   }
   choices <- as.character(utils::installed.packages()[, "Package"])
   package <- match.arg(package, choices)
-  suppressPackageStartupMessages(library((package), character.only=TRUE, quietly=TRUE))
+  library((package), character.only=TRUE, quietly=TRUE)
   suppressMessages(utils::help(package=(package), help_type="html"))
 }
