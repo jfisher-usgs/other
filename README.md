577565d31361942fd10fed986e90161a24f0c301
Jason Fisher <jfisher@usgs.gov>
2021-02-25 09:44:14 -0800

    Set option and add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# other
+
+R code to be run in a new R session. Add the following command to your `.Rprofile` file.
+
+```r
+source("https://raw.githubusercontent.com/jfisher-usgs/other/master/other.R")
+```
--- other.R
+++ other.R
@@ -1,12 +1,16 @@
+# set options
+options(Ncpus = max(1L, parallel::detectCores(logical = FALSE) - 1L))
+
+
 # Function to load package and open help documentation
 ph <- function(package) {
-  if (!tryCatch(is.character(package) && length(package) == 1L, error=function(e) FALSE)) {
-    package <- deparse(substitute(package), nlines=1)
+  if (!tryCatch(is.character(package) && length(package) == 1L, error = function(e) FALSE)) {
+    package <- deparse(substitute(package), nlines = 1)
   }
   choices <- as.character(utils::installed.packages()[, "Package"])
   package <- match.arg(package, choices)
-  library((package), character.only=TRUE, quietly=TRUE)
-  suppressMessages(utils::help(package=(package), help_type="html"))
+  library((package), character.only = TRUE, quietly = TRUE)
+  suppressMessages(utils::help(package = (package), help_type = "html"))
 }
 
 
